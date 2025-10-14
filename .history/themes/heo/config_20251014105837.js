db7e75f1291bac5358ab41a0d5e2c17ecf59b209
Gintmr <gintmr@GintmrdeMacBook-Air.local>
2025-10-14 11:21:45 +0800

    update 10.14-25

--- .history/themes/heo/config_20251014105837.js
+++ .history/themes/heo/config_20251014105837.js
@@ -4,105 +4,114 @@ const CONFIG = {
 
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2025-1-28', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: "Welcome to Gintmr's Blog", url: 'https://notion-page-1ir23d0ls-gintmrs-projects.vercel.app/' },    
-    { title: 'More information on my GitHub Page', url: 'https://github.com/gintmr' },  
+    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
+    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
   ],
+
   // 英雄区左右侧组件颠倒位置
   HEO_HERO_REVERSE: false,
   // 博客主体区左右侧组件颠倒位置
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: 'Daliy Record',  
-  HEO_HERO_TITLE_2: 'Work Record',    
-  HEO_HERO_TITLE_3: 'Interests',  
+  HEO_HERO_TITLE_1: '分享编程',
+  HEO_HERO_TITLE_2: '与思维认知',
+  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
+  HEO_HERO_TITLE_4: '新版上线',
+  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
+  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
   // 英雄区遮罩文字
-  HEO_HERO_COVER_TITLE: 'Wander···',    
+  HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: 'Daliy Record', url: '/tag/Daliy Record' },    
-  HEO_HERO_CATEGORY_2: { title: 'Work Record', url: '/tag/Work Record' },    
-  HEO_HERO_CATEGORY_3: { title: 'Interests', url: '/tag/Interests' },  
+  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
+  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
+  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-  HEO_HERO_RECOMMEND_POST_TAG: 'Recommend',
+  HEO_HERO_RECOMMEND_POST_TAG: '推荐',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
-  //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片    
+  //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    "Hello! 👇This is a Bulletin board.",
-
+    '你好！我是',
+    '🔍 分享与热心帮助',
+    '🤝 专修交互与设计',
+    '🏃 脚踏实地行动派',
+    '🏠 智能家居小能手',
+    '🤖️ 数码科技爱好者',
+    '🧱 团队小组发动机'
   ],
 
   // 个人资料底部按钮
-  HEO_INFO_CARD_URL1: 'xxx',  // 暂无
-  HEO_INFO_CARD_ICON1: 'fas fa-user',  
-  HEO_INFO_CARD_URL2: 'https://github.com/gintmr',  
-  HEO_INFO_CARD_ICON2: 'fab fa-github',  
-  HEO_INFO_CARD_URL3: 'https://gintmr.20250130.xyz',    
-  HEO_INFO_CARD_TEXT3: 'Back Home',    
+  HEO_INFO_CARD_URL1: '/about',
+  HEO_INFO_CARD_ICON1: 'fas fa-user',
+  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_ICON2: 'fab fa-github',
+  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
     {
-      title_1: '1',
-      img_1: 'https://ooo.0x0.ooo/2025/02/01/OG4UKU.jpg',  
+      title_1: 'AfterEffect',
+      img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp',
       color_1: '#989bf8',
-      title_2: '2',
-      img_2: 'https://ooo.0x0.ooo/2025/02/01/OG4T5p.jpg',  
+      title_2: 'Sketch',
+      img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp',
       color_2: '#ffffff'
     },
     {
-      title_1: '3',
-      img_1: 'https://ooo.0x0.ooo/2025/02/01/OG45Yj.jpg',  
+      title_1: 'Docker',
+      img_1: '/images/heo/20231108a540b2862d26f8850172e4ea58ed075102.webp',
       color_1: '#57b6e6',
-      title_2: '4',
-      img_2: 'https://ooo.0x0.ooo/2025/02/01/OG4bwv.jpg',  
+      title_2: 'Photoshop',
+      img_2: '/images/heo/2023e4058a91608ea41751c4f102b131f267075902.webp',
       color_2: '#4082c3'
     },
     {
-      title_1: '5',
-      img_1: 'https://ooo.0x0.ooo/2025/02/01/OG4WSY.jpg',
+      title_1: 'FinalCutPro',
+      img_1: '/images/heo/20233e777652412247dd57fd9b48cf997c01070702.webp',
       color_1: '#ffffff',
-      title_2: '6',
-      img_2: 'https://ooo.0x0.ooo/2025/02/01/OG4x1q.jpg',
+      title_2: 'Python',
+      img_2: '/images/heo/20235c0731cd4c0c95fc136a8db961fdf963071502.webp',
       color_2: '#ffffff'
     },
     {
-      title_1: '7',
-      img_1: 'https://ooo.0x0.ooo/2025/02/01/OG4npG.jpg',  
+      title_1: 'Swift',
+      img_1: '/images/heo/202328bbee0b314297917b327df4a704db5c072402.webp',
       color_1: '#eb6840',
-      title_2: '8',
-      img_2: 'https://ooo.0x0.ooo/2025/02/01/OG4ixM.jpg',  
+      title_2: 'Principle',
+      img_2: '/images/heo/2023f76570d2770c8e84801f7e107cd911b5073202.webp',
       color_2: '#8f55ba'
     },
     {
-      title_1: '9',
-      img_1: 'https://ooo.0x0.ooo/2025/02/01/OG4zDc.jpg',
+      title_1: 'illustrator',
+      img_1: '/images/heo/20237359d71b45ab77829cee5972e36f8c30073902.webp',
       color_1: '#f29e39',
-      title_2: '10',
-      img_2: 'https://ooo.0x0.ooo/2025/02/01/OG4F8r.jpg',  
+      title_2: 'CSS3',
+      img_2: '/images/heo/20237c548846044a20dad68a13c0f0e1502f074602.webp',
       color_2: '#2c51db'
     },
     {
-      title_1: '11',
-      img_1: 'https://ooo.0x0.ooo/2025/02/01/OG41f1.jpg',
+      title_1: 'JS',
+      img_1: '/images/heo/2023786e7fc488f453d5fb2be760c96185c0075502.webp',
       color_1: '#f7cb4f',
-      title_2: '12',
-      img_2: 'https://ooo.0x0.ooo/2025/02/01/OG4eKD.jpg',
+      title_2: 'HTML',
+      img_2: '/images/heo/202372b4d760fd8a497d442140c295655426070302.webp',
       color_2: '#e9572b'
     },
     {
-      title_1: '13',
-      img_1: 'https://ooo.0x0.ooo/2025/02/01/OG4N5I.jpg',
-      color_1: '#df5b40',  
-      title_2: '14',
-      img_2: 'https://ooo.0x0.ooo/2025/02/01/OG4Aw6.jpg',
+      title_1: 'Git',
+      img_1: '/images/heo/2023ffa5707c4e25b6beb3e6a3d286ede4c6071102.webp',
+      color_1: '#df5b40',
+      title_2: 'Rhino',
+      img_2: '/images/heo/20231ca53fa0b09a3ff1df89acd7515e9516173302.webp',
       color_2: '#1f1f1f'
     }
   ],
